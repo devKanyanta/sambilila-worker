e521e96f6d22dd9ba2f462a17546cda01bf57f60
devkanyanta <ctrophyjoseph@gmail.com>
2025-12-08 00:49:56 +0200

    Fixing the table cannot be found error

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,9 +1,13 @@
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-const pdfParse = require("pdf-parse");
+import {PDFParse} from 'pdf-parse';
 
 export async function extractTextFromPDF(buffer: Uint8Array): Promise<string> {
-  const data = await pdfParse(Buffer.from(buffer));
-  return data.text.trim();
+  try {
+    const parser = new PDFParse({ url: 'https://bitcoin.org/bitcoin.pdf' });
+
+	const result = await parser.getText();
+  return result.text;
+  } catch (err) {
+    console.error('PDF parse error:', err);
+    throw new Error('Failed to parse PDF');
+  }
 }
